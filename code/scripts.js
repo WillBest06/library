3edc7fc804f68e8c16f789716ab82761c0d916df
William Best <williambest133@gmail.com>
2025-04-18 16:20:28 +0100

    add delete and update read status functionality

--- code/scripts.js
+++ code/scripts.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+let myLibrary = [];
 const submitBtn = document.querySelector(".submit");
 const bookTable = document.querySelector(".book-table");
 const titleInput = document.querySelector("#title");
@@ -21,9 +21,9 @@ function addBookToLibrary() {
     let status ;
 
     if (readInput.checked == true)
-        status = 'yes';
+        status = true;
     else if (readInput.checked == false) {
-        status = 'no';
+        status = false;
     }
 
     const book = new Book(title, author, pages, status);
@@ -43,16 +43,87 @@ function tableUpdate() {
 
         for (let property in book) {
             const newCell = document.createElement('td');
-            newCell.textContent = book[property];
+
+            if (property === 'status') {
+                const readCheckBox = document.createElement('input');
+                readCheckBox.type = 'checkbox';
+
+                if (book[property] === true) {
+                    readCheckBox.checked = true;
+                    newRow.classList.toggle('read');
+                } else if (book[property] === false) {
+                    readCheckBox.checked = false;
+                    newRow.classList.toggle('unread');
+                }
+                        
+                readCheckBox.classList.toggle('readCHK')
+                newCell.appendChild(readCheckBox);  
+            } else {
+                newCell.textContent = book[property];
+            }
+            
             newRow.appendChild(newCell);
         }
+
+        const deleteCell = document.createElement('td');
+        const deleteBTN = document.createElement('button');
+        deleteBTN.textContent = 'delete';
+        deleteBTN.classList.toggle('deleteBTN');
+        deleteCell.appendChild(deleteBTN);
+        newRow.appendChild(deleteCell);
     }
 }
 
+function clearForm() {
+    titleInput.value = null;
+    authorInput.value = null;
+    pagesInput.value = null;
+    readInput.checked = false;
+}
+
+function deleteBook(e) {
+    const tableElement = e.target
+
+    if (tableElement.classList.contains('deleteBTN')) {
+        const bookToBeDeleted = e.target.parentElement.parentElement;
+        const IDofBookToBeDeleted = bookToBeDeleted.firstChild.textContent;
+        myLibrary = myLibrary.filter((book) => book.id != IDofBookToBeDeleted)
+        tableElement.closest('tr').remove();
+    };
+}
+
+function updateReadStatus(e) {
+    const tableElement = e.target
+
+    if (tableElement.classList.contains('readCHK')) {
+        const bookToBeUpdated = e.target.parentElement.parentElement;
+        const IDofBookToBeUpdated = bookToBeUpdated.firstChild.textContent;
+
+        myLibrary = myLibrary.map((book) => {
+            if (book.id === IDofBookToBeUpdated) {
+                book.status = !book.status;
+                colourChange(bookToBeUpdated);
+            }
+            
+            return book;
+        })
+    }
+}
+
+function colourChange (row) {
+    row.classList.toggle('read');
+    row.classList.toggle('unread');
+}
 
 
-submitBtn.addEventListener('click', (e) => {
+document.addEventListener('submit', (e) => {
     e.preventDefault();
     addBookToLibrary();
     tableUpdate();
+    clearForm();
+})
+
+bookTable.addEventListener('click', (e) => {
+    deleteBook(e);
+    updateReadStatus(e)
 })
